575629fff6b5fc402dd3b9b6e34aac19d321dae7
Uzeyir Sur <t.sur@hotmail.com>
2022-08-23 12:03:56 +0300

    MyOwnExercises

---
+++ src/chapters/chapter02/MyExercises/AlanıHesapla.java
@@ -0,0 +1,16 @@
+package chapters.chapter02.MyExercises;
+
+public class AlanıHesapla {
+	public static void main(String[] args) {
+		double yaricap;
+		double alan;
+
+		// yarıçapı oku
+		yaricap = 20;
+		// alanı hesapla
+		alan = yaricap * yaricap * 3.14159;
+
+		// alanı yazdır
+		System.out.println("yaricapi " + yaricap + " olan cemberin alani " + alan + " dir");
+	}
+}

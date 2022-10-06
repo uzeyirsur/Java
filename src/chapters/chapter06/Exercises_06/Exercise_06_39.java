32e044024a8fd71cba6a04b1ec2dcb35352ded67
Uzeyir Sur <t.sur@hotmail.com>
2022-10-07 02:21:29 +0300

    New Edits Exercises06

---
+++ src/chapters/chapter06/Exercises_06/Exercise_06_39.java
@@ -0,0 +1,36 @@
+package chapters.chapter06.Exercises_06;
+
+import java.util.Scanner;
+
+public class Exercise_06_39 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three points for p0, p1 and p2: ");
+        double x0 = input.nextDouble();
+        double y0 = input.nextDouble();
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        if (onTheLineSegment(x0, y0, x1, y1, x2, y2)) {
+            System.out.println("(" + x2 + ", " + y2 + ") is on the line segment from (" + x0 + ", " + y0 + ") to (" + x1 + ", " + y1 + ")");
+        } else if (onTheSameLine(x0, y0, x1, y1, x2, y2)) {
+            System.out.println("(" + x2 + ", " + y2 + ") is on the same line from (" + x0 + ", " + y0 + ") to (" + x1 + ", " + y1 + ")");
+
+        } else if (leftOfTheLine(x0, y0, x1, y1, x2, y2)) {
+            System.out.println("(" + x2 + ", " + y2 + ") is on the left side of the line from (" + x0 + ", " + y0 + ") to (" + x1 + ", " + y1 + ")");
+        }
+    }
+
+    public static boolean leftOfTheLine(double x0, double y0, double x1, double y1, double x2, double y2) {
+        return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) > 0;
+    }
+
+    public static boolean onTheSameLine(double x0, double y0, double x1, double y1, double x2, double y2) {
+        return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0;
+    }
+
+    public static boolean onTheLineSegment(double x0, double y0, double x1, double y1, double x2, double y2) {
+        return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0 && x0 < x2 && y0 < y2 && x1 > x2 && y1 > y2;
+    }
+}
---
+++ src/chapters/chapter07/Listings_07/FirstArrays.java
@@ -0,0 +1,36 @@
+package chapters.chapter07.Listings_07;
+
+public class FirstArrays {
+    public static void main(String[] args) {
+        int[] sayiDizisi;
+        sayiDizisi = new int[16];
+        sayiDizisi[0] = 1;
+        sayiDizisi[1] = 2;
+        sayiDizisi[2] = 1;
+        sayiDizisi[3] = 2;
+        sayiDizisi[4] = 1;
+        sayiDizisi[5] = 2;
+        sayiDizisi[6] = 1;
+        sayiDizisi[7] = 2;
+        sayiDizisi[8] = 1;
+        sayiDizisi[9] = 2;
+        sayiDizisi[10] = 1;
+        sayiDizisi[11] = 2;
+        sayiDizisi[12] = 1;
+        sayiDizisi[13] = 2;
+        sayiDizisi[14] = 1;
+        sayiDizisi[15] = 2;
+
+        for (int i = 0; i < sayiDizisi.length; i++){
+            System.out.println(sayiDizisi[i]);
+        }
+
+        double [] myList ={1,2,3,4};
+        for(int i = 0; i< myList.length; i++){
+            System.out.println(myList[i]);
+        }
+        char[] city = {'D', 'a', 'l', 'l','a', 's'};
+        System.out.println(city);
+
+    }
+}

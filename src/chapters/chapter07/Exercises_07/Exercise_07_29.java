310dd9239ae65b5070875ae3765d851d529fd282
Uzeyir Sur <t.sur@hotmail.com>
2022-10-19 02:18:11 +0300

    Up to Exercise_07 new edits

--- src/chapters/chapter07/Exercises_07/Exercise_07_29.java
+++ src/chapters/chapter07/Exercises_07/Exercise_07_29.java
@@ -1,51 +1,32 @@
 package chapters.chapter07.Exercises_07;
-                                          //not finished yet.
+
+//not finished yet.
 public class Exercise_07_29 {
     public static void main(String[] args) {
         int[] deck = new int[52];
-        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        int[] ranks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        makeDeck(deck);
+        int sum = 0;
+        int picks = 0;
+        while (sum != 24) {
+            int card1 = deck[(int) (Math.random() * 52)];
+            int card2 = deck[(int) (Math.random() * 52)];
+            int card3 = deck[(int) (Math.random() * 52)];
+            int card4 = deck[(int) (Math.random() * 52)];
+            sum = card1 + card2 + card3 + card4;
+            picks++;
+        }
+        System.out.println("The pick number " + picks + " yields the sum of 24");
+    }
+
+    public static void makeDeck(int[] deck) {
+        int cardNumber = 1;
         for (int i = 0; i < deck.length; i++) {
-            deck[i] = i;
-        }
-        for (int i = 0; i < deck.length; i++) {
-            int index = (int) (Math.random() * deck.length);
-            int temp = deck[i];
-            deck[i] = deck[index];
-            deck[index] = temp;
-        }
-        int[] selectedRanks = new int[4];
-        System.out.println("Selected cards are: ");
-        for (int i = 0; i < 4; i++) {
-            String suit = suits[deck[i] / 13];
-            selectedRanks[i] = ranks[deck[i] % 13];
-            System.out.print("Card number " + deck[i] + ": ");
-            switch (ranks[i]) {
-                case 1:
-                    System.out.println("Ace of " + suit);
-                    break;
-                case 11:
-                    System.out.println("Jack of " + suit);
-                    break;
-                case 12:
-                    System.out.println("Queen of " + suit);
-                    break;
-                case 13:
-                    System.out.println("King of " + suit);
-                    break;
-                default:
-                    System.out.println(selectedRanks[i] + " of " + suit);
+            deck[i] = cardNumber;
+            if (cardNumber == 13) {
+                cardNumber = 1;
+                continue;
             }
+            cardNumber++;
         }
-
-        for (int i = 0; i < selectedRanks.length - 1; i++) {
-            int sum = 0;
-            for (int j = i + 1; j < selectedRanks.length; j++) {
-                sum = selectedRanks[i] + selectedRanks[j];
-            }
-
-        }
-
-
     }
 }
---
+++ src/chapters/chapter07/Exercises_07/Exercise_07_34.java
@@ -0,0 +1,41 @@
+package chapters.chapter07.Exercises_07;
+
+import java.util.Scanner;
+
+public class Exercise_07_34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s = input.nextLine();
+        s = s.toLowerCase();
+        System.out.println(sort(s));
+    }
+
+    public static String sort(String s) {
+        char[] ch = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            ch[i] = s.charAt(i);
+        }
+
+        for (int i = 0; i < ch.length - 1; i++) {
+            char currentMin = ch[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < ch.length; j++) {
+                if (currentMin > ch[j]) {
+                    currentMin = ch[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                ch[currentMinIndex] = ch[i];
+                ch[i] = currentMin;
+            }
+
+        }
+        s = "";
+        for (int i = 0; i < ch.length; i++) {
+            s += ch[i];
+        }
+        return s;
+    }
+}

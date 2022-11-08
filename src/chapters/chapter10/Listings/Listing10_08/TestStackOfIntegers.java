cfe515c082ea68a973f41f5d5f030fd76282b913
Uzeyir Sur <t.sur@hotmail.com>
2022-11-08 03:37:12 +0300

    Chapter10 start

---
+++ src/chapters/chapter10/Listings/Listing10_08/TestStackOfIntegers.java
@@ -0,0 +1,15 @@
+package chapters.chapter10.Listings.Listing10_08;
+
+public class TestStackOfIntegers {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+
+        for(int i = 0; i < 10; i++){
+            stack.push(i);
+        }
+        while(!stack.empty()){
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
+}

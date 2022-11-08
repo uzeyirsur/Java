4cb4997f0e999beb6658676160ae3063fd452479
Uzeyir Sur <t.sur@hotmail.com>
2022-11-08 14:05:02 +0300

    Chapter10 start

---
+++ src/chapters/chapter10/CheckPoints/CheckPoint08.java
@@ -0,0 +1,17 @@
+package chapters.chapter10.CheckPoints;
+
+public class CheckPoint08 {
+    public static void main(String[] args) {
+        Integer i = new Integer("23");
+        Integer i2 = new Integer(23);
+        Integer i3 = Integer.valueOf("23");
+       Integer i4 = Integer.parseInt("23",8);
+       //Double d = new Double(); false because the wrapper classes do not have no-arg constructors.
+        Double d = Double.valueOf("23.45");
+        int i5 = (Integer.valueOf("23")).intValue();
+        double d2 =(Double.valueOf(23.4)).doubleValue();
+        int i6 =(Double.valueOf(23.4)).intValue();
+        String s = (Double.valueOf("23.4")).toString();
+
+    }
+}
---
+++ src/chapters/chapter10/CheckPoints/CheckPoint09.java
@@ -0,0 +1,26 @@
+package chapters.chapter10.CheckPoints;
+
+public class CheckPoint09 {
+    public static void main(String[] args) {
+
+        String s = 12 + "";
+        //or
+        Integer.toString(12);
+
+
+        Integer.valueOf("53");
+        //or
+        Integer.parseInt("53");
+
+
+        Double.toString(23);
+       //or
+        String s2 = 23.2 + "";
+
+
+       Double.valueOf("23.1");
+       //or
+        Double.parseDouble("23.1");
+
+    }
+}

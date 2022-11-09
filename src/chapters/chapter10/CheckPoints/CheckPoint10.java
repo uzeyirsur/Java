aee79eddf01eae7f32d83e05add72ee7b36875a9
Uzeyir Sur <t.sur@hotmail.com>
2022-11-09 03:29:07 +0300

    Chapter10 start

---
+++ src/chapters/chapter10/CheckPoints/CheckPoint10.java
@@ -0,0 +1,9 @@
+package chapters.chapter10.CheckPoints;
+
+public class CheckPoint10 {
+    public static void main(String[] args) {
+        Integer x = new Integer(3);
+        System.out.println(x.intValue());
+        System.out.println(x.compareTo(new Integer(4)));
+    }
+}

a58f1ad9611221ab7d4d6f527ace852daa441435
Uzeyir Sur <t.sur@hotmail.com>
2022-11-15 04:13:04 +0300

    Chapter10 Exercises

---
+++ src/chapters/chapter10/Exercises/Exercise14/MyDate.java
@@ -0,0 +1,39 @@
+package chapters.chapter10.Exercises.Exercise14;
+
+import java.util.GregorianCalendar;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+    MyDate(){
+        this(System.currentTimeMillis());
+    }
+    MyDate(long elapsedTime){
+setDate(elapsedTime);
+    }
+    MyDate(int year,int month,int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    public void setDate(long elapsedTime){
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(elapsedTime);
+        day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+        month = calendar.get(GregorianCalendar.MONTH);
+        year = calendar.get(GregorianCalendar.YEAR);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
---
+++ src/chapters/chapter10/Exercises/Exercise17/Exercise10_17.java
@@ -0,0 +1,20 @@
+package chapters.chapter10.Exercises.Exercise17;
+
+import java.math.BigInteger;
+
+public class Exercise10_17 {
+    public static void main(String[] args) {
+        long sqrt = (long)(Math.sqrt(Long.MAX_VALUE))+ 1;
+
+        BigInteger n = new BigInteger(sqrt +"");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(n.multiply(n));
+            n = n.add(BigInteger.ONE);
+
+        }
+
+
+
+    }
+}
